7a13a63e3e20ea852e3d40c1ea988021d715d189
WisnuRizki <akunpopo2@gmail.com>
2022-06-01 09:21:20 +0700

    merubah  app terkoneksi database

--- routers/todo.router.js
+++ routers/todo.router.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const {getToDo, postToDo} = require('../controllers/todo.controller')
+const {getToDo, postToDo, updateToDo,deleteTodo} = require('../controllers/todo.controller')
 
 router.get('/', getToDo);
 router.post('/', postToDo);
+router.put('/:id', updateToDo);
+router.delete('/:id', deleteTodo);
 
-module.exports = router ;
+module.exports = router;
